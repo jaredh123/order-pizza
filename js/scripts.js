c39b6388ccfd0d042501982606839b6d9aa65c2a
jaredh123 <jared.hanson12345@gmail.com>
2019-02-01 11:55:38 -0800

    Add basic html page info, css, and start js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,28 @@
-$(document).ready(function() {
+function xxx(ccc, ccc, ccc) {
+  this.xx,
+  this.xx,
+  this.xx
+}
 
+xxx.prototype.xx =function() {
+  return this.xxxx + this.xxxxx + this.xxxxxx;
+}
+
+topping 1
+cheese
+
+size 1 2
+sm  lg
+
+
+
+$(document).ready(function() {
+  $("#run").submit(function() {
+    var xx = xx;
+
+
+    alert(price);
+  });
 });
+
+$("#result")
